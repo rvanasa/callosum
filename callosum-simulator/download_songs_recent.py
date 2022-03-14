938b061cbeeaed1456f20e5a69b0de87dd0ed1a1
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-03-14 16:57:28 -0600

    Add download pipeline for Spotify top charts

--- callosum-simulator/download_songs_recent.py
+++ callosum-simulator/download_songs_recent.py
@@ -1,5 +1,6 @@
 import prelude
 from visualizer import load_spectrogram
+from spotify import get_features
 
 from pydub import AudioSegment
 import numpy as np
@@ -16,71 +17,68 @@ def standardize(x):
     return (x - x.mean()) / x.std()
 
 
-df_charts = pd.read_csv('https://spotifycharts.com/regional/global/daily/latest/download')
-
-print(df_charts)
+# https://spotifycharts.com/regional/global/daily/latest/download
+df_charts = pd.read_csv('data/regional-global-daily-latest.csv', skiprows=1)
+df_charts['track_id'] = df_charts.URL.str[31:]
+df_charts.set_index('track_id', inplace=True)
 
 if __name__ == '__main__':
 
-    groups = df.sort_values('popularity', ascending=False).drop_duplicates(['artist_name']).groupby('genre')
-    for genre, g in groups:
-        for key, row in g[:40].iterrows():
-            try:
-                name = f'spotify/{key}'
-                genre = row.genre
-                artist = row.artist_name
-                song = row.track_name
-                url = f'ytsearch:{artist} topic - {song}'  # 'topic' keyword prioritizes "Topic" music channels
+    for track_id, chart_row in df_charts.iterrows():
+        try:
+            name = f'charts/{track_id}'
+            genre = 'Recent'
+            artist = chart_row['Artist']
+            song = chart_row['Track Name']
 
-                print(name)  ##
-                if os.path.exists(f'music/{name}.mp3'):
-                    continue  # Already downloaded
+            row = get_features(name, genre, track_id)
 
-                download_ext = 'm4a'
-                download_file = f'music_cache/download/{name}.{download_ext}'
-                with YoutubeDL({
-                    # 'extract-audio': True,
-                    'format': 'bestaudio[ext=m4a]',
-                    # 'outtmpl': f'music_cache/download_{name}',
-                    'outtmpl': download_file,
-                }) as dl:
-                    df_rel = df[(df.artist_name == artist) & (df.track_name == song)]
-                    row = df_rel.iloc[0]
+            # genre = row.genre
+            url = f'ytsearch:{artist} topic - {song}'  # 'topic' keyword prioritizes "Topic" music channels
 
-                    info = dl.extract_info(url, download=False)
-                    if info.get('_type') == 'playlist':
-                        info = info['entries'][0]
+            print(name)  ##
+            if os.path.exists(f'music/{name}.mp3'):
+                continue  # Already downloaded
 
-                    # print(list(info.keys()))
-                    print(f'{name} :: {artist} - {song}')
-                    print(info['title'])
-                    # print(info['description'])
+            download_ext = 'm4a'
+            download_file = f'music_cache/download/{name}.{download_ext}'
+            with YoutubeDL({
+                # 'extract-audio': True,
+                'format': 'bestaudio[ext=m4a]',
+                # 'outtmpl': f'music_cache/download_{name}',
+                'outtmpl': download_file,
+            }) as dl:
+                # df_rel = df[(df.artist_name == artist) & (df.track_name == song)]
+                # row = df_rel.iloc[0]
 
-                    if not os.path.exists(download_file):
-                        dl.extract_info(url, download=True)
+                info = dl.extract_info(url, download=False)
+                if info.get('_type') == 'playlist':
+                    info = info['entries'][0]
 
-                    (AudioSegment
-                     .from_file(download_file)
-                     .export(f'music/{name}.mp3', format='mp3')
-                     )
+                # print(list(info.keys()))
+                print(f'{name} :: {artist} - {song}')
+                print(info['title'])
+                # print(info['description'])
 
-                    df_features = pd.read_csv('music_features.csv')
-                    df_features = pd.concat([
-                        df_features[df_features.name != name],
-                        pd.DataFrame([dict(
-                            name=name,
-                            genre=genre,
-                            artist=artist,
-                            song=song,
-                            **{key: row[key] for key in keys}
-                        )])
-                    ])
-                    df_features.to_csv('music_features.csv', index=False)
+                if not os.path.exists(download_file):
+                    dl.extract_info(url, download=True)
 
-                    load_spectrogram(name)  # Precompute spectrogram
+                (AudioSegment
+                 .from_file(download_file)
+                 .export(f'music/{name}.mp3', format='mp3')
+                 )
 
-            # time.sleep(5)
+                df_features = pd.read_csv('music_features.csv')
+                df_features = pd.concat([
+                    df_features[df_features.name != name],
+                    pd.DataFrame([row])
+                ])
+                df_features.to_csv('music_features.csv', index=False)
 
-            except Exception as err:
-                print(err)
-                time.sleep(60)
+                load_spectrogram(name)  # Precompute spectrogram
+
+        # time.sleep(5)
+
+        except Exception as err:
+            print(err)
+            time.sleep(60)
--- callosum-simulator/spotify.py
+++ callosum-simulator/spotify.py
@@ -10,26 +10,61 @@ sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
     client_secret=os.getenv('SPOTIFY_SECRET'),
 ))
 
-df_tracks = pd.DataFrame(sp.search('Conrad Sewell Hold Me Up Throttle', type='track')['tracks']['items'])
-# df_tracks[:3]
 
-print(len(df_tracks))  ##
+def get_features(name, genre, track_id):
+    # df_tracks = pd.DataFrame(sp.search('Conrad Sewell Hold Me Up Throttle', type='track')['tracks']['items'])
+    # df_tracks[:3]
 
-track = df_tracks.iloc[0]
+    # print(len(df_tracks))  ##
 
-artist = track.artists[0]['name']
-name = track['name']
+    # track = df_tracks.iloc[0]
 
-print(artist, '-', name)
+    track = pd.Series(sp.track(track_id))
 
-df_features = pd.read_csv('music_features.csv')
+    artist = track['artists'][0]['name']
+    name = track['name']
 
-# print(','.join(track[[df_features.columns]]))
+    # print(artist, '-', name)
 
-all_features = sp.audio_features(track.id)
-print(len(all_features))
-features = all_features[0]
+    # df_features = pd.read_csv('music_features.csv')
 
-print(','.join(str(features[s]) for s in ['danceability', 'energy', 'liveness', 'valence']))
+    # print(','.join(track[[df_features.columns]]))
 
-# analysis = sp.audio_analysis(track.id)
+    all_features = sp.audio_features(track_id)
+    # print(len(all_features))
+    features = pd.Series(all_features[0])
+
+    row = pd.Series(dict(
+        name=name,
+        genre=genre,
+        artist=artist,
+        song=name,
+        **{k: features[k] for k in ['danceability', 'energy', 'liveness', 'valence']}
+    ))
+    return row
+
+
+if __name__ == '__main__':
+    df_tracks = pd.DataFrame(sp.search('Conrad Sewell Hold Me Up Throttle', type='track')['tracks']['items'])
+    # df_tracks[:3]
+
+    print(len(df_tracks))  ##
+
+    track = df_tracks.iloc[0]
+
+    artist = track.artists[0]['name']
+    name = track['name']
+
+    print(artist, '-', name)
+
+    df_features = pd.read_csv('music_features.csv')
+
+    # print(','.join(track[[df_features.columns]]))
+
+    all_features = sp.audio_features(track.id)
+    print(len(all_features))
+    features = all_features[0]
+
+    print(','.join(str(features[s]) for s in ['danceability', 'energy', 'liveness', 'valence']))
+
+    # analysis = sp.audio_analysis(track.id)
